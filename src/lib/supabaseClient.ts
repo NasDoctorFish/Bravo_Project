3038003b4f02d2f0b991f3f2553929a417da1f9e
akamanlev <missmaypde@gmail.com>
2026-05-12 13:54:15 +0800

    testing

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,3 @@
-console.log('Supabase URL:', import.meta.env.VITE_SUPABASE_URL)
-console.log('Supabase ANON Key:', import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY)
-
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_URL
